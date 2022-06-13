41e37387b335147046acb591c8a15d2f707a82a6
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-06-13 18:20:07 +0700

    update router

--- src/components/Token/index.jsx
+++ src/components/Token/index.jsx
@@ -4,11 +4,40 @@ import ProgressBar from 'react-bootstrap/ProgressBar'
 
 export default function Token({percent,date}) {
 
+    const lockHours = 2232;
+    const lockTimestamp = 1654945381;
+
+    const secondsToDhms = (seconds) => {
+        seconds = Number(seconds);
+        var d = Math.floor(seconds / (3600*24));
+        var h = Math.floor(seconds % (3600*24) / 3600);
+        var m = Math.floor(seconds % 3600 / 60);
+        var s = Math.floor(seconds % 60);
+
+        var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+        var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+        var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+        var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+        return dDisplay + hDisplay + mDisplay + sDisplay;
+    }
+
+
+    console.log(new Date(lockTimestamp*1000));
+
+    secondsToDhms(1654945381)
+
   return (
     <div className="col-xl-6">
         <div className="token">
-            <div className="token-title">
-                Unlock countdown
+            
+            <div className="wapper">
+                <div className="token-title">
+                    Unlock countdown
+                </div>
+
+                <div className="token-total">
+                    90000000
+                </div>
             </div>
 
             <div className="token-process">
--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Admin.scss'
 
 import Token from '../../components/Token'
 
 export default function Admin() {
+   
+    
+
   return (
     <div className="admin">
         <div className="container">
@@ -12,9 +15,14 @@ export default function Admin() {
                     AR FOREX TOKEN LOCKED EVENTS
                </div>
 
+
                <div className="col-xl-12 admin-logo centering">
                    <img src="./img/logo.png" alt="" />
                </div>
+
+                <div className="col-xl-12 admin-address-wallet">
+                    378687234873687x3768736483276478
+                </div>
 
                <div className="col-xl-12 admin-subtitle">
                    Locked AFT Tokens - 15,000,000 AFT (15%)
